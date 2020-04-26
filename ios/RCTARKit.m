76178844140450bd8b8bf12ae9e2e4f1ec35ccd1
Jack Sobocinski <sobojack@bowst.com>
2020-04-26 17:54:23 -0400

    add refernce image

--- ios/RCTARKit.m
+++ ios/RCTARKit.m
@@ -325,7 +325,7 @@ static RCTARKit *instance = nil;
                     CGImageRef cgImage = [uiimage CGImage];
                     ARReferenceImage *image = [[ARReferenceImage alloc] initWithCGImage:cgImage orientation:kCGImagePropertyOrientationUp physicalWidth:0.1];
                     // NSData * arImage = ARReferenceImage.init(newImage, orientation: up, physicalWidth: 0.1);
-                    detectionImagesSet = [detectionImagesSet setByAddingObjectsFromSet:image;
+                    detectionImagesSet = [detectionImagesSet setByAddingObjectsFromSet:image];
 
                 }
             
