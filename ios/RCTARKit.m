b801f6664e3dda7296db5b553c8b5b00d3a80a19
Jack Sobocinski <sobojack@bowst.com>
2020-05-10 14:15:56 -0400

    update scale for pinch

--- ios/RCTARKit.m
+++ ios/RCTARKit.m
@@ -595,17 +595,17 @@ static NSDictionary * getPlaneHitResult(NSMutableArray *resultsMapped, const CGP
         recognizer.state == UIGestureRecognizerStateChanged || 
         recognizer.state == UIGestureRecognizerStateEnded) {
 
-        recognizer.view.transform = CGAffineTransformScale(recognizer.view.transform, recognizer.scale, recognizer.scale);
-        recognizer.scale = 1;
+        // recognizer.view.transform = CGAffineTransformScale(recognizer.view.transform, recognizer.scale, recognizer.scale);
+        // recognizer.scale = 1;
 
-        // if(self.onPinchGesture) {
-        //     NSDictionary *pinchGesture = @{
-        //             @"scale": @(recognizer.scale),
-        //             @"velocity": @(recognizer.velocity)
-        //             };
+        if(self.onPinchGesture) {
+            NSDictionary *pinchGesture = @{
+                    @"scale": @(CGAffineTransformScale(recognizer.view.transform, recognizer.scale, recognizer.scale)),
+                    @"velocity": @(recognizer.velocity)
+                    };
 
-        //     self.onPinchGesture(pinchGesture);
-        // }
+            self.onPinchGesture(pinchGesture);
+        }
     }
 
 }
