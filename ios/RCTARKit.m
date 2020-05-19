d5faf02f1e44b8a9ee8fdf703cf295a1849b936c
Jack Sobocinski <sobojack@bowst.com>
2020-05-18 22:34:43 -0400

    upadte pan handler

--- ios/RCTARKit.m
+++ ios/RCTARKit.m
@@ -625,8 +625,8 @@ static NSDictionary * getPlaneHitResult(NSMutableArray *resultsMapped, const CGP
             float slideFactor = 0.1 * slideMult; // Increase for more of a slide
             CGPoint finalPoint = CGPointMake(recognizer.view.center.x + (velocity.x * slideFactor), 
                                         recognizer.view.center.y + (velocity.y * slideFactor));
-            finalPoint.x = MIN(MAX(finalPoint.x, 0), self.arView.bounds.size.width);
-            finalPoint.y = MIN(MAX(finalPoint.y, 0), self.arView.bounds.size.height);
+            // finalPoint.x = MIN(MAX(finalPoint.x, 0), self.arView.bounds.size.width);
+            // finalPoint.y = MIN(MAX(finalPoint.y, 0), self.arView.bounds.size.height);
             NSDictionary *panGesture = @{
                     @"x": @(finalPoint.x),
                     @"y": @(finalPoint.y)
