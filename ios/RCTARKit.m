4faf85f7096e1a197c102a1d346401bb3b2f79f8
Jack Sobocinski <sobojack@bowst.com>
2020-05-31 14:20:12 -0400

    log distance

--- ios/RCTARKit.m
+++ ios/RCTARKit.m
@@ -381,6 +381,7 @@ static RCTARKit *instance = nil;
             distanceTransform.columns[3][0] = 0;
             distanceTransform.columns[3][1] = 0;
             distanceTransform.columns[3][2] = distance;
+            NSLog("distance:", distance)
 
 
             // double startLat = GLKMathDegreesToRadians(locationLat);
@@ -397,7 +398,7 @@ static RCTARKit *instance = nil;
 
 
             return  @{
-                        @"results": distance
+                        @"results": @"test"
                     };
         }
     }
