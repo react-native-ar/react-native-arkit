a961701bd6ef1d0fdc81da8e424e2cd2975a2a08
Jack Sobocinski <sobojack@bowst.com>
2020-05-31 11:37:26 -0400

    initn number with float

--- ios/RCTARKit.m
+++ ios/RCTARKit.m
@@ -350,7 +350,8 @@ static RCTARKit *instance = nil;
     if(options[@"landmarkLat"]){
         if(options[@"landmarkLong"]){
 
-            NSNumber * nsLandMarkLat = options[@"landmarkLat"];
+            // NSNumber * nsLandMarkLat = options[@"landmarkLat"];
+            NSNumber * nsLandMarkLat = [[NSNumber alloc] initWithFloat:[[options objectForKey:@"landmarkLat"] doubleValue]];
             NSNumber * nsLandMarkLong = options[@"landmarkLong"];
             NSNumber * nsLocationLat = options[@"locationLat"];
             NSNumber * nsLocationLong = options[@"locationLong"];
