a2df16143ddf4f73f97e73331be965980eab431b
Jack Sobocinski <sobojack@bowst.com>
2020-06-15 20:21:49 -0400

    add transformation matrix

--- ios/RCTARKit.m
+++ ios/RCTARKit.m
@@ -413,10 +413,10 @@ static RCTARKit *instance = nil;
     matrix_float4x4 translationMatrix = matrix_identity_float4x4;
     translationMatrix.columns[3] = position;
     matrix_float4x4 rotationMatrix = matrix_identity_float4x4;        
-    float rotationMatrix.columns.[0].x = cos(bearing);
-    float rotationMatrix.columns.[0].z = -sin(bearing);
-    float rotationMatrix.columns.[2].x = sin(bearing);
-    float rotationMatrix.columns.[2].z = cos(bearing);
+    rotationMatrix.columns.[0][2] = cos(bearing);
+    rotationMatrix.columns.[0][2] = -sin(bearing);
+    rotationMatrix.columns.[2][2] = sin(bearing);
+    rotationMatrix.columns.[2][2] = cos(bearing);
 
 
     simd_float4x4 transformMatrix = simd_mul(rotationMatrix, translationMatrix);
