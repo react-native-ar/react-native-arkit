27233b51f71c65ba8d9be3457fbc1847cdc37cb1
Jack Sobocinski <sobojack@bowst.com>
2020-06-15 22:25:16 -0400

    log anchor:

--- ios/RCTARKit.m
+++ ios/RCTARKit.m
@@ -422,7 +422,7 @@ static RCTARKit *instance = nil;
     simd_float4x4 transformMatrix = simd_mul(rotationMatrix, translationMatrix);
     ARAnchor *localAnchor = [[ARAnchor alloc] initWithTransform:transformMatrix];
 
-    NSLog(@"%@", localAnchor.transform);
+    NSLog(@"%@", localAnchor);
 
     // float opposite = landmark.altitude - location.altitude;
     // float tilt = atan2(opposite, distance);
