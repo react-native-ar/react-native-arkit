6675c802cf775342deb22e4240656cf5db1e9d71
Jack Sobocinski <sobojack@bowst.com>
2020-06-22 18:30:59 -0400

    access rotation matrix

--- ios/RCTARKit.m
+++ ios/RCTARKit.m
@@ -419,17 +419,16 @@ static RCTARKit *instance = nil;
     float finalX =  (distance * sin(bearing)) / sin(dynamicDegrees);
 
     simd_float4 position = simd_make_float4(0.0, 0.0, -distance, 0.0);
-    matrix_float4x4 translationMatrix = matrix_identity_float4x4;
-    translationMatrix.columns[3] = position;
-    matrix_float4x4 rotationMatrix = matrix_identity_float4x4;        
-    rotationMatrix.columns[0][2] = cos(bearing);
-    rotationMatrix.columns[0][2] = -sin(bearing);
-    rotationMatrix.columns[2][2] = sin(bearing);
-    rotationMatrix.columns[2][2] = cos(bearing);
+    matrix_float4x4 distanceTransform = matrix_identity_float4x4;
+    distanceTransform.columns[3] = position;
+
+    float opposite = 13.655269622802734 - 13.655269622802734
+    float  tilt = atan2(opposite, distance)
+
+    GLKMatrix4 rotation = GLKMatrix4MakeXRotation(tilt);
+    float firstRotation = rotation[0][0];
 
 
-
-    simd_float4x4 transformMatrix = simd_mul(rotationMatrix, translationMatrix);
     ARAnchor *localAnchor = [[ARAnchor alloc] initWithTransform:transformMatrix];
 
 
