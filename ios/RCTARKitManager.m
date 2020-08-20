4e7902a17be06349f4c05a03623008a20d49e367
Jack Sobocinski <sobojack@bowst.com>
2020-08-19 21:40:18 -0400

    try arview

--- ios/RCTARKitManager.m
+++ ios/RCTARKitManager.m
@@ -465,7 +465,7 @@ RCT_EXPORT_METHOD(createImageObject:(RCTPromiseResolveBlock)resolve reject:(RCTP
     paintingNode.eulerAngles = angles;
     paintingNode.position = position;
     
-    [self.rootNode addChildNode:paintingNode];
+    [self.arView.scene.rootNode addChildNode:paintingNode];
 }
 
 
