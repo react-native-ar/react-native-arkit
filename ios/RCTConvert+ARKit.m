63eb6f8777b02be591577332ab30875be7abb9e5
Jack Sobocinski <sobojack@bowst.com>
2020-08-25 21:59:44 -0400

    add material var

--- ios/RCTConvert+ARKit.m
+++ ios/RCTConvert+ARKit.m
@@ -152,11 +152,10 @@
 
 + (SCNImageNode *)SCNImageNode:(id)json {
     SCNPlane *planeGeometry = [SCNPlane planeWithWidth:0.5 height:0.5];
-    NSDictionary* shape = json[@"shape"];
+    SCNMaterial *material = [SCNMaterial material];
 
     UIImage *image = [UIImage imageWithContentsOfFile:@"https://arworldimages.s3.us-east-2.amazonaws.com/%2F00c3e640-9232-11ea-8c4f-59f384dfd412.png"];
     material.diffuse.contents = image;
-
     NSMutableArray *materials = [NSMutableArray array];
     [materials addObject:@(material)];
 
